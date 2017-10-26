227ebac188c578c26350dec9fa98112379c2d736
xuezier <xzj15859722542@hotmail.com>
2017-10-26 12:09:18 +0800

    add event lock

--- src/ProcessEvent.js
+++ src/ProcessEvent.js
@@ -6,6 +6,8 @@ function ProcessEvent(fun) {
   this._eventMap = new Map();
   this._emited = new Map();
   this._eventIdGrow = 0;
+  this._running = new Map();
+  this._waiting = new Map();
   this._catch = undefined;
 
   var self = this;
@@ -32,13 +34,68 @@ ProcessEvent.prototype.register = function(eventName, event) {
  * @param {String|Number} eventName
  * @param data
  */
-ProcessEvent.prototype.emit = function(eventName, data) {
-  var eventId = this._eventIdGrow++;
+ProcessEvent.prototype.emit = function(process, eventName, data) {
+  var self = this;
+  if (process instanceof ProcessEvent) {
+    self = ProcessEvent;
+  } else {
+    data = eventName;
+    eventName = process;
+  }
 
-  return this._run(eventId, eventName, data);
+  var eventId = self._eventIdGrow++;
+
+  if (self._emited.has(eventId)) {
+    var _emitedName = self._emited.get(eventId);
+
+    if (_emitedName === eventName) return;
+    else return self._emit(eventName, data);
+  }
+
+  if (self._running.has(eventName)) return self._pushWaiting(eventId, eventName, data);
+
+  else return self._run(eventId, eventName, data);
+};
+
+ProcessEvent.prototype._pushWaiting = function(eventId, eventName, data) {
+  var next = { eventId: eventId, data: data };
+
+  if (this._waiting.has(eventName)) {
+    var eventWaiting = this._waiting.get(eventName);
+    eventWaiting.push(next);
+    this._waiting.set(name, eventWaiting);
+  } else {
+    this._waiting.set(eventName, [next]);
+  }
+
+  return this;
 };
 
 ProcessEvent.prototype._emit = ProcessEvent.prototype.emit;
+
+/**
+ * lock event running
+ * @param {String|Number} eventName
+ */
+ProcessEvent.prototype._lockRun = function(eventName) {
+  if (this._eventMap.has(eventName))
+    this._running.set(eventName, true);
+};
+
+/**
+ * unlock after event finished running
+ * @param {String|Number} eventName
+ */
+ProcessEvent.prototype._unlock = function(eventName) {
+  var eventWaiting = this._waiting.get(eventName);
+
+  if (this._running.has(eventName) && eventWaiting && eventWaiting.length) {
+    var next = eventWaiting.shift();
+    this._run(next.eventId, eventName, next.data);
+  } else {
+    this._running.delete(eventName);
+  }
+};
 
 /**
  * run event
@@ -47,22 +104,28 @@ ProcessEvent.prototype._emit = ProcessEvent.prototype.emit;
  * @param data
  */
 ProcessEvent.prototype._run = function(eventId, eventName, data) {
-  if (this._emited.has(eventId)) {
-    var _emitedName = this._emited.get(eventId);
-
-    if (_emitedName === eventName) return;
-    else return this._emit(eventName, data);
-  }
+  this._lockRun(eventName);
 
   var event = this._eventMap.get(eventName);
 
   if (event instanceof Function) {
-    try {
-      event.call(this, data);
-      this._emited.set(eventId, eventName);
-    } catch (e) {
-      this._runCatch(eventName, e);
-    }
+    var self = this;
+    setTimeout(function() {
+      try {
+        var next = event.call(self, data);
+        self._emited.set(eventId, eventName);
+        if (next instanceof Promise) {
+          next.catch(function(nextCatchedError) {
+            if (nextCatchedError)
+              self._runCatch(eventName, nextCatchedError);
+          });
+        } else {
+          self._unlock(eventName);
+        }
+      } catch (e) {
+        self._runCatch(eventName, e);
+      }
+    });
   } else {
     this._runCatch(eventName, new Error(eventName + ' is not a registered event'));
   }
@@ -75,6 +138,8 @@ ProcessEvent.prototype._run = function(eventId, eventName, data) {
  * @param {Error} error
  */
 ProcessEvent.prototype._runCatch = function(eventName, error) {
+  this._unlock(eventName);
+
   if (!(error instanceof Error)) return;
   if (this._catch instanceof Function) {
     console.error('event ' + eventName + ' error');
@@ -91,6 +156,7 @@ ProcessEvent.prototype._runCatch = function(eventName, error) {
  */
 ProcessEvent.prototype.catch = function(fun) {
   this._catch = fun.bind(this);
+  return this;
 };
 
 module.exports = ProcessEvent;
